75466d8ed75ef85b7df72ef63d25da04f5303cb2
Denwo <tywood@pdx.edu>
2017-07-06 03:05:44 -0700

    map updates
    UI improvements

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/MainActivity.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/MainActivity.java
@@ -1,22 +1,31 @@
 package com.groupc.officelocator;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private static int SPLASH_TIME_OUT = 1500;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-
-            Intent intent = new Intent(MainActivity.this, splash.class);
-            startActivity(intent);
-            finish();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(MainActivity.this, campus.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_TIME_OUT);
 
 
     }
