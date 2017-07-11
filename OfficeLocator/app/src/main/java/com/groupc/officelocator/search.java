49f9054189d848d0af11df68a1a230814cfd466b
Nhi Vu <vunhi@pdx.edu>
2017-07-11 16:51:09 -0700

    Added "No matching results" message for search listview

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/search.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/search.java
@@ -34,6 +34,7 @@ public class search extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         searchList = (ListView)findViewById(R.id.searchList);
+        searchList.setEmptyView(findViewById(R.id.empty));
         searchBar = (EditText)findViewById(R.id.searchBar);
         adapter = new ArrayAdapter<String>(this,R.layout.list_items,R.id.textView,locations);
         searchList.setAdapter(adapter);
