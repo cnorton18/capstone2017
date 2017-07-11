37b53f7d6de93c3a32fed05aeb0456c5401997ec
Nhi Vu <vunhi@pdx.edu>
2017-07-10 19:08:24 -0700

    most up-to-date branch

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/search.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/search.java
@@ -31,7 +31,6 @@ public class search extends AppCompatActivity {
         searchBar = (EditText)findViewById(R.id.searchBar);
         adapter = new ArrayAdapter<String>(this,R.layout.list_items,R.id.textView,locations);
         searchList.setAdapter(adapter);
-        searchList.setVisibility((adapter.isEmpty())?View.GONE:View.VISIBLE);
 
         searchBar.addTextChangedListener(new TextWatcher(){
 
