015e55f1d5a76bc2a1a0d026afbbec0784551ef0
Nhi Vu <vunhi@pdx.edu>
2017-07-13 00:56:42 -0700

    changed error in roomsearch spinner + building location and changed buildinghighlight images

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/floorplan.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/floorplan.java
@@ -4,6 +4,7 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,11 +19,17 @@ import java.util.TimerTask;
 
 public class floorplan extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    //public Button back;  - Took out the back button
+    //track which building
+    public static int buildingselected = 0;
+
+    public Button maplocationbut;
+    public Button back;
     ImageView floorPlanImage;
     private Spinner chooseRoom;
     int[] buildingNames = {R.array.miaHamm, R.array.tigerWoods};//, R.array.danFouts, R.array.tigerWoods, R.array.nolanRyan};
     private boolean isFirstSelection = true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class floorplan extends AppCompatActivity implements AdapterView.OnItemSe
         actionBar.hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floorplan);
+
         Intent goToFloorPlan = getIntent();
 
         //Displaying the correct building name and floorplan image
@@ -45,15 +53,6 @@ public class floorplan extends AppCompatActivity implements AdapterView.OnItemSe
         floorPlanImage = (ImageView) findViewById(R.id.floorPlanImage);
         floorPlanImage.setImageResource(res);
 
-        /*
-        //Back button
-        back = (Button) findViewById(R.id.floorplanbutton1);
-        back.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                finish();
-            }}
-        );
-        */
 
         int spinnerNumber = goToFloorPlan.getIntExtra("spinnerNumber",0);
         chooseRoom = (Spinner) findViewById(R.id.roomSelector);
@@ -61,7 +60,60 @@ public class floorplan extends AppCompatActivity implements AdapterView.OnItemSe
         chooseRoom.setAdapter(adapter);
         chooseRoom.setOnItemSelectedListener(this);
         chooseRoom.setSelection(0);
+
+
+        //mia hamm
+        if(spinnerNumber == 0){ buildingselected = 1;}
+
+        //tiger woods
+        else if(spinnerNumber == 1){ buildingselected = 2;}
+
+
+
+
+
+        //Back button
+        back = (Button) findViewById(R.id.floorplanbutton1);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent theintent = new Intent(floorplan.this, campus.class);
+                startActivity(theintent);
+            }}
+        );
+
+
+
+        //map location button
+        maplocationbut = (Button) findViewById(R.id.floorplanbutton2);
+        maplocationbut.setOnClickListener(new View.OnClickListener(){
+
+                                              public void onClick(View v){
+
+                                                  if(buildingselected == 1) {
+                                                      Log.d("ok", "mia hamm");
+                                                      Intent theintent = new Intent(floorplan.this, building1location.class);
+                                                      startActivity(theintent);
+                                                  } else if (buildingselected == 2) {
+                                                      Log.d("ok", "tiger woods");
+                                                      Intent theintent = new Intent(floorplan.this, building2location.class);
+                                                      startActivity(theintent);
+                                                  }
+
+                                              }
+
+                                          }
+        );
+
+
     }
+
+
+
+
+
+
+
+
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
@@ -69,14 +121,44 @@ public class floorplan extends AppCompatActivity implements AdapterView.OnItemSe
             isFirstSelection = false;
             return;
         }
+
+
         TextView myText = (TextView) view;
-        Intent goToRoom = new Intent(floorplan.this, directions.class);
-        goToRoom.putExtra("roomName", myText.getText());
+        //Intent goToRoom = new Intent(floorplan.this, directions.class);
+        //goToRoom.putExtra("roomName", myText.getText());
+
         if(myText.getText().equals("Choose a room")) {
             return;
         }
-        startActivity(goToRoom);
+
+
+
+        //startActivity(goToRoom);
+
+
+        if(buildingselected == 0) {
+
+            if (buildingselected == 1) {
+                floorPlanImage.setImageResource(R.drawable.miahamm);
+            } else if (buildingselected == 2) {
+                floorPlanImage.setImageResource(R.drawable.tigerwoods);
+            }
+        }
+
+        if(buildingselected == 1) {
+    if(i == 1){ floorPlanImage.setImageResource(R.drawable.flyknit);}
+    else if(i == 2){ floorPlanImage.setImageResource(R.drawable.airmax);}
+}
+else if(buildingselected == 2) {
+    if(i == 1){ floorPlanImage.setImageResource(R.drawable.airjordan);}
+    else if(i == 2){ floorPlanImage.setImageResource(R.drawable.roshe);}
+}
+
     }
+
+
+
+
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView){
