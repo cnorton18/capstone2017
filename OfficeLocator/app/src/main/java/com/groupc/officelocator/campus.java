662049be08090d3e11e7404742d95d6211996e9f
AnarchySong <anarchysong@gmail.com>
2017-08-08 11:50:48 -0700

    test

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 //import android.widget.CheckBox;
-
+//this is a test
 public class campus extends AppCompatActivity {
 
     //Buttons for just 2 buildings
