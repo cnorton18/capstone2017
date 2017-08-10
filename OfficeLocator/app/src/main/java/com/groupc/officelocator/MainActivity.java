5eb8b7f5fcaeb3fe9da57a399f04ba8dc7beb92c
ZachIsMe <zrp@pdx.edu>
2017-08-09 19:56:38 -0700

    changed fonts
    removed Impact, which was licensed, and replaced with open source font

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/MainActivity.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.*;
 
 public class MainActivity extends AppCompatActivity {
 private static int SPLASH_TIME_OUT = 1500;
@@ -12,9 +14,14 @@ private static int SPLASH_TIME_OUT = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        if(actionBar!=null) {
+            actionBar.hide();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       /* TextView tv = (TextView) findViewById(R.id.logoText);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/LeagueGothic-Regular.ttf");
+        tv.setTypeface(tf);*/
 
         //Splash Screen delay
         new Handler().postDelayed(new Runnable(){
