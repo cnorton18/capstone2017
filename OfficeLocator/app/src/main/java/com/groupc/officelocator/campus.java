efb3370181c4dcec0176238250feac7470faf542
vdiego <vdiego@pdx.edu>
2017-08-19 09:55:14 -0700

    added ken griffey jr

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
@@ -17,13 +17,13 @@ import android.support.design.widget.BottomNavigationView;
 public class campus extends AppCompatActivity {
         RelativeLayout relativeLayout;
 
-        //Buttons for just 2 buildings
-        public Button miaHamm, tigerWoods, albertoSalazar, boJackson, danFouts, jerryRice,
-                joanBenoitSamuelson, johnMcEnroe, globe;
-        Button[] arrayButtons = {miaHamm, tigerWoods, albertoSalazar, boJackson, danFouts,
-                jerryRice, joanBenoitSamuelson, johnMcEnroe};
-        int[] buttonNames = {R.id.miahamm, R.id.tigerwoods, R.id.albertosalazar, R.id.bojackson,
-                R.id.danfouts, R.id.jerryrice, R.id.joanbenoitsamuelson, R.id.johnmcenroe};
+        public Button albertoSalazar, boJackson, danFouts, jerryRice, joanBenoitSamuelson,
+                johnMcEnroe, kenGriffeyJr, miaHamm, tigerWoods, globe;
+        Button[] arrayButtons = {albertoSalazar, boJackson, danFouts, jerryRice, joanBenoitSamuelson,
+                johnMcEnroe, kenGriffeyJr, miaHamm, tigerWoods};
+        int[] buttonNames = {R.id.albertosalazar, R.id.bojackson, R.id.danfouts, R.id.jerryrice,
+                R.id.joanbenoitsamuelson, R.id.johnmcenroe, R.id.kengriffeyjr, R.id.miahamm,
+                R.id.tigerwoods};
         public mapdata data;
         public ImageButton satelliteview;
         private static int globesetting = 0;
