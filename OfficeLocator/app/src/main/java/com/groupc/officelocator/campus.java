b41f3b953d2afb8d89f74f61d54aff652c78f12b
vdiego <vdiego@pdx.edu>
2017-08-18 23:38:00 -0700

    added bo jackson and fixed campus buttons for resolution

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
@@ -18,9 +18,9 @@ public class campus extends AppCompatActivity {
         RelativeLayout relativeLayout;
 
         //Buttons for just 2 buildings
-        public Button miaHamm, tigerWoods, albertoSalazar, globe;
-        Button[] arrayButtons = {miaHamm, tigerWoods, albertoSalazar};
-        int[] buttonNames = {R.id.miahamm, R.id.tigerwoods, R.id.albertosalazar};
+        public Button miaHamm, tigerWoods, albertoSalazar, boJackson, globe;
+        Button[] arrayButtons = {miaHamm, tigerWoods, albertoSalazar, boJackson};
+        int[] buttonNames = {R.id.miahamm, R.id.tigerwoods, R.id.albertosalazar, R.id.bojackson};
         public mapdata data;
         public ImageButton satelliteview;
         private static int globesetting = 0;
