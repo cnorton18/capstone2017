89540a89c4e986c775fcb58f622d4ed884c3fb86
vdiego <vdiego@pdx.edu>
2017-08-19 11:14:31 -0700

    added steve prefontaine hall

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
@@ -19,14 +19,14 @@ public class campus extends AppCompatActivity {
 
         public Button albertoSalazar, boJackson, danFouts, jerryRice, joanBenoitSamuelson,
                 johnMcEnroe, kenGriffeyJr, miaHamm, michaelJordan, mikeSchmidt, nikeSportsCenter, nolanRyan,
-                peteSampras, tigerWoods, globe;
+                peteSampras, stevePrefontaineHall, tigerWoods, globe;
         Button[] arrayButtons = {albertoSalazar, boJackson, danFouts, jerryRice, joanBenoitSamuelson,
                 johnMcEnroe, kenGriffeyJr, miaHamm, michaelJordan, mikeSchmidt, nikeSportsCenter,
-                nolanRyan, peteSampras, tigerWoods};
+                nolanRyan, peteSampras, stevePrefontaineHall, tigerWoods};
         int[] buttonNames = {R.id.albertosalazar, R.id.bojackson, R.id.danfouts, R.id.jerryrice,
                 R.id.joanbenoitsamuelson, R.id.johnmcenroe, R.id.kengriffeyjr, R.id.miahamm,
                 R.id.michaeljordan, R.id.mikeschmidt, R.id.nikesportscenter, R.id.nolanryan,
-                R.id.petesampras, R.id.tigerwoods};
+                R.id.petesampras, R.id.steveprefontainehall, R.id.tigerwoods};
         public mapdata data;
         public ImageButton satelliteview;
         private static int globesetting = 0;
